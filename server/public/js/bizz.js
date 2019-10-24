6f7bd0eb46d2f22b6108a9450bf4d487ca242076
Donald Polansky <d.polansky@zahavat.com>
2019-10-24 09:36:28 -0500

    upload js for dev purposes

--- server/public/js/bizz.js
+++ server/public/js/bizz.js
@@ -1,25 +1,91 @@
-import { Script } from "vm";
+// import { Script } from "vm";
 
 $(document).ready(function() {
 
-// Below might be coming up null due to document.ready as a function and not a set Script.
+var config = {
+    ".kickAPI": {},
+    ".kickAPI-deselect": {
+      allow_single_deselect: true
+    },
+    ".kickAPI-no-single": {
+      disable_search_threshold: 10
+    },
+    ".kickAPI-no-results": {
+      no_results_text: "Oops, nothing found!"
+    },
+    ".kickAPI-width": {
+      width: "95%"
+    }
+  };
+
+  for (var selector in config) {
+    $(selector).chosen(config[selector]);
+  }
+
+  // Capture the form inputs
+  $("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    // Form validation
+    function validateForm() {
+      var isValid = true;
+      $(".form-control").each(function() {
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+
+      $(".kickAPI").each(function() {
+
+        if ($(this).val() === "") {
+          isValid = false;
+        }
+      });
+      return isValid;
+    }
+
+    // If all required fields are filled
+    if (validateForm()) {
+      // Create an object for the user"s data
+      var orderData = {
+        order: $("#order_number").val(),
+      };
+
+      // AJAX post the data to the orders API.
+      $.get("/api/orders", orderData, function(data) {
+
+        // Grab the result from the AJAX post so that the best match's order and info are displayed.
+        $("#match-order").text(data.order);
+        // $("#match-img").attr("src", data.photo);
+
+        // Show the modal with the best match
+        $("#results-modal").modal("toggle");
+
+      });
+    } else {
+      alert("Please fill out all fields before submitting.");
+    }
+  });
+});
+
+
+      // Below might be coming up null due to document.ready as a function and not a set Script.
 
     // let q_order = $("#order_search").val().trim();
     // console.log(q_order);
 
-    $(".submit").on("click", function(event) {
-        event.preventDefault();
+    // $(".submit").on("click", function(event) {
+    //     event.preventDefault();
 
-        const cust_order = document.getElementById("q_order");
-        console.log(cust_order);
+    //     const cust_order = document.getElementById("q_order");
+    //     console.log(cust_order);
         // $.ajax({
         //     method: "GET",
         //     url: "../order/" + cust_order
         // }).then(function(data) {
         //     location.reload();
         // });
-    });
-    });
+    // });
 
     
 // function kickAPI(data){
@@ -42,3 +108,4 @@ $(document).ready(function() {
 //     });
 //     XHR.open('POST', 'https://')
 // }
+
